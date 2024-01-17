0db4c4943eae083e1e7f88c3caa8d155a72e64f2
Fariz Akbar <77776418+Fariz313@users.noreply.github.com>
2024-01-17 14:54:22 +0700

    done

---
+++ pages/home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container n-container-primary">
+    <h1 class="text-center w-100 mb-3">Dashboard</h1>
+
+    <div class="d-flex justify-content-end mb-3">
+      <div class="n-bg-secondary card w-50 border-0 p-3">
+        <p class="text-center">
+          Selamat datang di era revolusi pertanian Indonesia!
+        </p>
+      </div>
+    </div>
+
+    <div class="d-flex mb-3">
+      <div class="n-bg-secondary card w-50 border-0 p-3">
+        <p class="text-center">Dapat memantau PH tanah</p>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-end mb-3">
+      <div class="n-bg-secondary card w-50 border-0 p-3">
+        <p class="text-center">Dapat memantau Kelembapan Tanah</p>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-end mb-3">
+      <div class="n-bg-secondary card w-50 border-0 p-3">
+        <p class="text-center">Dapat memantau ketinggian air sawah</p>
+      </div>
+    </div>
+  </div>
+</template>
